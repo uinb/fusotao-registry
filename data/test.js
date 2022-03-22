56a8b7bbf2100e25476ff0ddb7285d646eb37abc
lavender78 <lindawu8134@gmail.com>
2022-03-22 15:59:52 +0800

    feat: add site config

---
+++ data/test.js
@@ -0,0 +1,20 @@
+export default {
+  "fxdx-vodka-107":{
+    name: "fxdx-vodka-107",
+    website: 'https://testnet.fxdx.finance',
+    logo: require('../assets/fxdx_logo.svg'),
+    address: '5DcvPdFpS6dxUbYkNZvAkw9pyQgwUkzUUX9kEXNg1PZbJuUY',
+  },
+  "fxdx-testnet":{
+    name: "fxdx-testnet",
+    website: 'https://testnet.fxdx.finance',
+    logo: require('../assets/fxdx_logo.svg'),
+    address: '5EZxQHTqTabKzgDMwyM4mE84R5ABStm9n9sKXnFK65x3Tdj7',
+  },
+  "fxdx":{
+    name: "fxdx",
+    website: 'https://testnet.fxdx.finance',
+    logo: require('../assets/fxdx_logo.svg'),
+    address: '5GZJY2evJJ6GGeEMAdaf3rXyuZa7tXj5C9oopLvYnMnWaVuX',
+  }
+}
